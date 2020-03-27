bf1be9ad6dd36d4c9de04ff9cf0e2423aefb5f54
chienvh <chien.vh@gmail.com>
2020-03-28 01:59:37 +0700

    Added new code of Create first Asp.Net MVC Database code first.

---
+++ MyFirstDatabaseCodeFirstMVC/Context/ProductManagementContext.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using MyFirstDatabaseCodeFirstMVC.Models;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace MyFirstDatabaseCodeFirstMVC.Context
+{
+    public class ProductManagementContext:DbContext
+    {
+        /*
+         * The name of the connection string is passed into the constructor.
+         * This name will be used in web.config file
+         */
+        public ProductManagementContext() : base("ProductManagementContextDB")
+        { }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+        /*
+         * This code will remove the pluralizing convention that is the default behavior
+         * for all model builders --> You will then be able to create tables with singular names.
+         */
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
---
+++ MyFirstDatabaseCodeFirstMVC/Models/Category.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyFirstDatabaseCodeFirstMVC.Models
+{
+    public class Category
+    {
+        /*
+         * The ID property will become the primary key column of the database table.
+         * By default, the EF interprets a property named ID or 'classname' ID as the primary key.
+         * Eg: Create primary key: Id or CategoryId
+         */
+        public int Id { get; set; }
+        public string CategoryName { get; set; }
+
+        public virtual ICollection<Product> Products { get; set; }
+    }
+}
